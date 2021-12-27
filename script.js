97671b8be1fd400a839dddfe83a064d9f75ec1dc
Bobeer95 <artur.buja2@gmail.com>
2021-12-27 17:31:14 +0100

    Adding semicolons

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const liters = document.getElementById("litters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
-updateBigCup()
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx));
-})
+});
 
 function highlightCups(idx) {
     if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
@@ -20,25 +20,26 @@ function highlightCups(idx) {
         } else {
             cup.classList.remove('full')
         }
-    })
-    updateBigCup()
+    });
+    updateBigCup();
 }
-function updateBigCup(){
+
+function updateBigCup() {
     const fullCups = document.querySelectorAll('.cup-small.full').length;
     const totalCups = smallCups.length;
-    if (fullCups === 0){
+    if (fullCups === 0) {
         percentage.style.visibility = 'hidden';
         percentage.style.height = 0;
     } else {
         percentage.style.visibility = 'visible';
-        percentage.style.height = `${fullCups / totalCups * 330}px`
-        percentage.innerText = `${fullCups / totalCups * 100}%`
+        percentage.style.height = `${ fullCups / totalCups * 330 }px`
+        percentage.innerText = `${ fullCups / totalCups * 100 }%`
     }
-    if( fullCups === totalCups){
+    if (fullCups === totalCups) {
         remained.style.visibility = 'hidden';
         remained.style.height = 0;
     } else {
         remained.style.visibility = 'visible';
-        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+        liters.innerText = `${ 2 - (250 * fullCups / 1000) }L`
     }
 }
